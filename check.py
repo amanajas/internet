e3d1b14364c9ebd9ff5cfd6e9f27691283d0a735
Thiago Amanajas <4821968+amanajas@users.noreply.github.com>
2021-04-01 23:23:33 +0200

    Exception handler

--- check.py
+++ check.py
@@ -7,23 +7,26 @@ from datetime import datetime
 
 
 def check_internet(file="internet.xlsx"):
-    if not os.path.exists(file):
-        writer = pd.ExcelWriter(file)
-        pd.DataFrame({'Date': [], 'Time': [], 'Speed': []}).to_excel(writer, 'base', index=False)
-        writer.save()
-        print("Excel file created!")
-    df = pd.read_excel(file)
-    s = speedtest.Speedtest()
-    current_date = datetime.now().strftime('%d/%m/%Y')
-    current_time = datetime.now().strftime('%H:%M')
-    speed = s.download(threads=None)*(10**-6)
-    df.loc[len(df)] = {'Date': current_date, 'Time': current_time, 'Speed': speed}
-    print("Log: ", df.loc[len(df)-1])
-    df.to_excel(
-        file,
-        sheet_name='base',
-        index=False
-    )
+    try:
+        if not os.path.exists(file):
+            writer = pd.ExcelWriter(file)
+            pd.DataFrame({'Date': [], 'Time': [], 'Speed': []}).to_excel(writer, 'base', index=False)
+            writer.save()
+            print("Excel file created!")
+        df = pd.read_excel(file)
+        s = speedtest.Speedtest()
+        current_date = datetime.now().strftime('%d/%m/%Y')
+        current_time = datetime.now().strftime('%H:%M')
+        speed = s.download(threads=None)*(10**-6)
+        df.loc[len(df)] = {'Date': current_date, 'Time': current_time, 'Speed': speed}
+        print("Log: ", df.loc[len(df)-1])
+        df.to_excel(
+            file,
+            sheet_name='base',
+            index=False
+        )
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
